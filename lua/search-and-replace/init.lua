0a1548d220a8acddfa952339dfd25aff28322fb8
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-22 03:59:37 -0300

    CHORE: algorithm to search and replace

---
+++ lua/search-and-replace/init.lua
@@ -0,0 +1,34 @@
+local words_to_table = require 'search-and-replace.utils.words_to_table'
+local run_command = require 'search-and-replace.utils.run_command'
+
+
+local function search_and_replace_each_one(args)
+  local args_to_table = words_to_table(args)
+  local word = args_to_table[1]
+  local new_word = args_to_table[3]
+  local replace_command = "%s/" .. word .. "/" .. new_word .. "/gc"
+  vim.cmd(replace_command)
+end
+
+local function search_and_replace_all(args)
+  local args_to_table = words_to_table(args)
+  local word = args_to_table[1]
+  local new_word = args_to_table[3]
+  local replace_command = "%s/" .. word .. "/" .. new_word .. "/g"
+  vim.cmd(replace_command)
+end
+
+local function setup_commands()
+  run_command('SearchAndReplaceAll', function(data) search_and_replace_all(data.args) end, {
+    nargs = "*"
+  })
+  run_command('SearchAndReplaceAllEachOne', function(data) search_and_replace_each_one(data.args) end, {
+    nargs = "*"
+  })
+end
+
+local function start()
+  setup_commands()
+end
+
+return start
